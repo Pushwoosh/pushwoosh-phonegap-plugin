d2fcbb20f852b8424fcaaf114ec3ac0e5c82ef4c
EddyVerbruggen <eddyverbruggen@gmail.com>
2014-05-12 16:09:37 +0200

    WP8 support (work in progress)

---
+++ src/wp8/PWUserData.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Phone.Shell;
+using PushSDK;
+using WPCordovaClassLib.Cordova;
+using WPCordovaClassLib.Cordova.Commands;
+
+namespace Cordova.Extension.Commands
+{
+    public class PWUserData : BaseCommand
+    {
+        private static NotificationService NotificationService
+        {
+            get { return ((PhonePushApplicationService)PhoneApplicationService.Current).NotificationService; }
+        }
+
+        public void GetUserData(string options)
+        {
+           DispatchCommandResult(new PluginResult(PluginResult.Status.OK, NotificationService.UserData));
+        }
+    }
+}
---
+++ src/wp8/PWUserToken.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Phone.Shell;
+using PushSDK;
+using PushSDK.Classes;
+using WPCordovaClassLib.Cordova;
+using WPCordovaClassLib.Cordova.Commands;
+
+namespace Cordova.Extension.Commands
+{
+    public class PWUserToken : BaseCommand
+    {
+        private static NotificationService NotificationService
+        {
+            get { return ((PhonePushApplicationService) PhoneApplicationService.Current).NotificationService; }
+        }
+
+        public void GetUserToken(string options)
+        {
+            if (!string.IsNullOrEmpty(NotificationService.PushToken))
+                DispatchCommandResult(new PluginResult(PluginResult.Status.OK, NotificationService.PushToken));
+
+            NotificationService.OnPushTokenUpdated += OnPushTokenUpdated;
+        }
+
+        private void OnPushTokenUpdated(object sender, CustomEventArgs<Uri> e)
+        {
+            DispatchCommandResult(new PluginResult(PluginResult.Status.OK, e.Result.ToString()));
+        }
+    }
+}
