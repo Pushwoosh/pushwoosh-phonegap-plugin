596fa69748a329628c73b472277e98958945eebe
Jake Visser <jake@nightrise.com.au>
2016-06-29 20:50:37 -0700

    no message

--- src/android/src/com/pushwoosh/plugin/pushnotifications/PushNotifications.java
+++ src/android/src/com/pushwoosh/plugin/pushnotifications/PushNotifications.java
@@ -63,7 +63,7 @@ public class PushNotifications extends CordovaPlugin
 	public static final String GET_HWID = "getPushwooshHWID";
 	public static final String GET_LAUNCH_NOTIFICATION = "getLaunchNotification";
 
-    String [] permissions = { Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION };
+    String [] permissions = { android.permission.ACCESS_COARSE_LOCATION, android.permission.ACCESS_FINE_LOCATION };
     
 	boolean receiversRegistered = false;
 	boolean broadcastPush = true;
