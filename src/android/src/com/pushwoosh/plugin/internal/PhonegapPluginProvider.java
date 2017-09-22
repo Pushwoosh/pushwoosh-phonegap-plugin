d3965c057224c3e3091fa3696a5d924a72f9d74d
Mikhail Savin <smu@arello-mobile.com>
2017-09-22 10:39:20 +0700

    Update to Pushwoosh Android sdk 5.1.1

---
+++ src/android/src/com/pushwoosh/plugin/internal/PhonegapPluginProvider.java
@@ -0,0 +1,17 @@
+package com.pushwoosh.plugin.internal;
+
+import com.pushwoosh.internal.PluginProvider;
+
+public class PhonegapPluginProvider implements PluginProvider {
+	@Override
+	public String getPluginType() {
+		try {
+			//If contains class Cordova than it's cordova plugin
+			Class.forName("org.apache.cordova.CordovaPlugin");
+			return "Cordova";
+		} catch (ClassNotFoundException ignore) {
+      //Otherwise this is PhoneGap build
+			return "PhoneGap Build";
+		}
+	}
+}
