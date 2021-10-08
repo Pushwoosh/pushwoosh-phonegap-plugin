f5d9ea69e9a49bf802d51c79256ca4cabf39e28f
Nikolay Galizin <ngalizin@pushwoosh.com>
2021-10-08 20:15:17 +0700

    [feature][PUSH-27868] fix agconnect-services hook

--- hooks/outsystems/handle_agconnect_services.js
+++ hooks/outsystems/handle_agconnect_services.js
@@ -11,6 +11,7 @@ var COMMENT = "//This line is added by cordova-plugin-hms-push plugin";
 var NEW_LINE = "\n";
 var APP_BUILD_GRADLE_FILE = "platforms/android/app/build.gradle";
 var APPLY_PLUGIN = "apply plugin: 'com.huawei.agconnect'"
+var PLUGIN_BUILD_GRADLE_FILE = "libs/android/googleservices-build.gradle";
 var HUAWEI_PUSH_KIT_DEPENDENCY = "implementation \"com.huawei.hms:push:5.3.0.304\""
 
 function getZipFile(resourcesFolder, prefZipFilename) {
@@ -140,20 +141,27 @@ function updateAppBuildGradle(file) {
         var appGradleContent = FSUtils.readFile(file, "UTF-8");
         if (appGradleContent.indexOf(APPLY_PLUGIN) === -1) {
             appGradleContent = appGradleContent + NEW_LINE + APPLY_PLUGIN;
+            
             FSUtils.writeFile(file, appGradleContent);
         }
-        if (appGradleContent.indexOf("com.huawei.hms:push") === -1) {
-            var dependenciesLastIndex = appGradleContent.lastIndexOf("// SUB-PROJECT DEPENDENCIES START");
+    }
+}
 
-            appGradleContent = 
-                appGradleContent.substring(0, dependenciesLastIndex + 33) + 
+function updatePluginBuildGradle(file) {
+    if (FSUtils.exists(file)) {
+        var pluginGradleContent = FSUtils.readFile(file, "UTF-8");
+        if (pluginGradleContent.indexOf("com.huawei.hms:push") === -1) {
+            var dependenciesLastIndex = pluginGradleContent.lastIndexOf("dependencies {");
+
+            pluginGradleContent = 
+                pluginGradleContent.substring(0, dependenciesLastIndex + 14) + 
                 NEW_LINE + 
                 HUAWEI_PUSH_KIT_DEPENDENCY +
                 NEW_LINE + 
-                appGradleContent.substring(dependenciesLastIndex + 34);
-            
-                console.log(appGradleContent);
-            FSUtils.writeFile(file, appGradleContent);
+                pluginGradleContent.substring(dependenciesLastIndex + 15);
+
+            console.log(pluginGradleContent);
+            FSUtils.writeFile(file, pluginGradleContent);
         }
     }
 }
@@ -205,7 +213,7 @@ module.exports = function(context) {
             updateRepositoriesGradle(ROOT_REPOSITORIES_GRADLE_FILE);
             updateRepositoriesGradle(APP_REPOSITORIES_GRADLE_FILE);
             updateAppBuildGradle(APP_BUILD_GRADLE_FILE);
-
+            updatePluginBuildGradle(PLUGIN_BUILD_GRADLE_FILE);
         }
         return resolve();
     });
